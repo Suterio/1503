e129b5b0c2d3dadb743ff5593a9cde738792b78c
Suterio <gabriel_suterio@hotmail.com>
2018-03-19 22:26:36 -0300

    inicio array

--- Escola.java
+++ Escola.java
@@ -1,13 +1,20 @@
 public class Escola{
     public static void main (String args[]){
     
-    Pessoa p = new Pessoa(args[0],args[1]);
-    p.mostraDados();
-    
-    Aluno a = new Aluno(args[0],args[1],Long.parseLong(args[2]));
-    a.mostraDados();
-    
-    Professor prof = new Professor(args[0],args[1],args[2],Integer.parseInt(args[3]));
-    prof.mostraDados();    
+        Pessoa p = new Pessoa(args[0],args[1]);
+        p.mostraDados();
+        
+        Aluno a = new Aluno(args[0],args[1],Long.parseLong(args[2]));
+        a.mostraDados();
+        
+        Professor prof = new Professor(args[0],args[1],args[3],Integer.parseInt(args[4]));
+        prof.mostraDados();    
     }
+    
+    public static void testArrayList(){
+        
+    }
+
+
+
 }
